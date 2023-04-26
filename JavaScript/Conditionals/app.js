eaddc12569b20bf789544c214d7c1ea5b25a414d
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-04-26 20:41:53 +0900

    Commit on 0426 20:40

--- JavaScript/Conditionals/app.js
+++ JavaScript/Conditionals/app.js
@@ -8,3 +8,25 @@ if (random >= 0.5) {
   console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
   console.log(random);
 }
+
+// const dayOfWeek = prompt("Enter a Day").toLowerCase();
+
+// if (dayOfWeek === "monday") {
+//   console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === "saturday") {
+//   console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === "friday") {
+//   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//   console.log("MEH");
+// }
+
+// const age = 3;
+
+// if (age < 5) {
+//   console.log("YOU ARE A BABY YOU GET IN FOR FREE!");
+// } else if (age < 10) {
+//   console.log("YOU ARE A CHILD YOU PAY 10$");
+// } else if (age < 65) {
+//   console.log("YOU ARE AN ADULT. YOU PAY $20");
+// }
