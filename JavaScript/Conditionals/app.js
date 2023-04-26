895903694f6643026da436029b15dbc48ca30fce
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-04-26 19:25:37 +0900

    Commit on 0426/ 19:25

--- JavaScript/Conditionals/app.js
+++ JavaScript/Conditionals/app.js
@@ -1,6 +1,10 @@
-console.log("BEFORE CONDITIONAL");
 let random = Math.random();
 if (random < 0.5) {
   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+  console.log(random);
 }
-console.log("AFTER CONDITIONAL");
+
+if (random >= 0.5) {
+  console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+  console.log(random);
+}
