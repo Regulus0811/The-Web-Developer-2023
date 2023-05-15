0318f1d5b62d7cb7ae2a8de8de58740fd3776378
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-05-15 14:51:15 +0900

    Commit on 0515/14:51

--- JavaScript/Conditionals/app.js
+++ JavaScript/Conditionals/app.js
@@ -1,25 +1,25 @@
-let random = Math.random();
-if (random < 0.5) {
-  console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-  console.log(random);
-}
-
-if (random >= 0.5) {
-  console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-  console.log(random);
-}
-
-// const dayOfWeek = prompt("Enter a Day").toLowerCase();
-
-// if (dayOfWeek === "monday") {
-//   console.log("UGHHH I HATE MONDAYS!");
-// } else if (dayOfWeek === "saturday") {
-//   console.log("YAY I LOVE SATURDAYS!");
-// } else if (dayOfWeek === "friday") {
-//   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
-// } else {
-//   console.log("MEH");
+// let random = Math.random();
+// if (random < 0.5) {
+//   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//   console.log(random);
 // }
+
+// if (random >= 0.5) {
+//   console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//   console.log(random);
+// }
+
+// // const dayOfWeek = prompt("Enter a Day").toLowerCase();
+
+// // if (dayOfWeek === "monday") {
+// //   console.log("UGHHH I HATE MONDAYS!");
+// // } else if (dayOfWeek === "saturday") {
+// //   console.log("YAY I LOVE SATURDAYS!");
+// // } else if (dayOfWeek === "friday") {
+// //   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// // } else {
+// //   console.log("MEH");
+// // }
 
 // const age = 3;
 
@@ -29,4 +29,20 @@ if (random >= 0.5) {
 //   console.log("YOU ARE A CHILD YOU PAY 10$");
 // } else if (age < 65) {
 //   console.log("YOU ARE AN ADULT. YOU PAY $20");
+// } else {
+//   console.log("you are a senior. you pay 10$");
 // }
+
+const Password = prompt("please enter a new password");
+
+// Password must be 6+ characters
+if (Password.length >= 6) {
+  if (Password.indexOf(" ") === -1) {
+    console.log("Valid Password!");
+  } else {
+    console.log("Password cannot contain spaces!");
+  }
+} else {
+  console.log("PASSWORD TOO SHORT! Must be 6+ characters!");
+}
+// Password cannot include space
