1d123ace8652ef7b004c299c568a13a3f18210da
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-06-28 21:40:13 +0900

    Commit on 2023-06-28 21:40

--- JavaScript/CallBack/ArrowFunc.js
+++ JavaScript/CallBack/ArrowFunc.js
@@ -13,3 +13,36 @@
 // const rollDie = () => {
 //   return Math.floor(Math.random * 6) + 1;
 // };
+
+// const rollDie = () => Math.floor(Math.random() * 6) + 1;
+
+// const add = (a, b) => a + b;
+
+const movies = [
+  {
+    title: "Amadeus",
+    score: 99,
+  },
+  {
+    title: "Stand by me",
+    score: 85,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+  },
+  {
+    title: "Alien",
+    score: 90,
+  },
+];
+
+// const newMovies = movies.map(function (movie) {
+//   return `${movie.title} - ${movie.score}`;
+// });
+
+// const newMovies = movies.map((movie) => `${movie.title} - ${movie.score / 10}`);
+
+const newMovies = movies.map((movie) => {
+  return `${movie.title} - ${movie.score}`;
+});
